62ec11c94200e45791dae65d77f3156310e4e848
cesarcalvo171-cyber <cesarcalvo171@gmail.com>
2026-07-07 14:59:52 -0600

    config: conectar a nueva base de datos Supabase de zapateria

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fldcsbnxgrsihyltljhh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZGNzYm54Z3JzaWh5bHRsamhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNTU2NjUsImV4cCI6MjA5NzczMTY2NX0.Dmm0VdP5TnqOqR-U7DOyGm1lmWDJ8JSYsfCorzhoOZQ';
+const supabaseUrl = 'https://byvdyzvmelqynvffetmt.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5dmR5enZtZWxxeW52ZmZldG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NTEwODcsImV4cCI6MjA5OTAyNzA4N30.zEclXcRef_oiObhg_VzvnjYKLs5Uw-ev7JhfGxnO-yY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
